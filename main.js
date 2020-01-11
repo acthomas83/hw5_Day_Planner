329795a85b6cd55b22b26b19704339d97114e283
Aaron Thomas <acthomas83@gmail.com>
2020-01-10 22:04:47 -0500

    added date and time function.

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-moment().format('LT');
-console.log(moment)
+// display day and date under header text
+var dateTime = document.getElementById("#date-time");
+var time = moment().format('h:mm:ss a');
+var date = moment().format('MMMM Do YYYY');
+var day = moment().format('dddd');
+console.log(time);
+console.log(date);
+console.log(day);
 
-// $().on("click", save-btn) {
-//     localStorage.setItem(<input>.val</input>);
-// }
+var div = document.getElementById("day-date");
+div.innerHTML += day + ", " + date;
+
+var myVar = setInterval(myTimer, 1000);
+
+function myTimer() {
+    var time = moment().format('h:mm:ss a');
+    var clock = moment.unix(Number);
+  console.log(clock);
+  
+//   THIS IS WHERE I GOT STUCK
+if(clock >'9:00:00 a') {
+    document.getElementById("nine-am").style.color = "red";
+}}
+
+// set css of event blocks to be red at present time, green in future, and gray in the past
+// Strore values of each event block in localStorage when clicking save button.
